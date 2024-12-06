11675422d22604f6e19bbf99d231a58e799b7394
clre20 <167730039+clre20@users.noreply.github.com>
2024-12-06 16:34:38 +0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,8 @@
+echo Installing required Python packages...
+pip install flask flask_login flask-sqlalchemy paho-mqtt
+
+echo Cloning the GitHub repository...
+git clone https://github.com/clre20/Earthquake-power-system-2024-Topics.git
+
+echo Installation and download complete!
+pause
