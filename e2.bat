79e36f31227bb67f6cd5c4769fdb149b4c8787fc
clre20 <167730039+clre20@users.noreply.github.com>
2024-12-19 10:29:10 +0800

    Delete e2.bat

--- e2.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-pip install flask flask_login flask-sqlalchemy paho-mqtt
-timeout /t 5
-powershell -Command "Invoke-WebRequest -Uri https://github.com/clre20/Earthquake-power-system-2024-Topics/archive/refs/heads/main.zip -OutFile Earthquake-power-system-2024-Topics.zip"
-powershell -Command "Expand-Archive -Path Earthquake-power-system-2024-Topics.zip -DestinationPath ."
-del Earthquake-power-system-2024-Topics.zip
-pause
